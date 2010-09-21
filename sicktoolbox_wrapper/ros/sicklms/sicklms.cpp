f3e284ad56b06ec7b20d6a96f03f4d9aa6847a65
blaise <blaise@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-09-21 06:51:16 +0000

    Applied patch from #4290 with a small tweak. Still needs testing before release.
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/laser_drivers/trunk@32800 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- sicktoolbox_wrapper/ros/sicklms/sicklms.cpp
+++ sicktoolbox_wrapper/ros/sicklms/sicklms.cpp
@@ -31,36 +31,33 @@
 
 #include <csignal>
 #include <cstdio>
+#include <math.h>
 #include <sicklms-1.0/SickLMS.hh>
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 using namespace SickToolbox;
 using namespace std;
 
-void publish_scan(ros::Publisher *pub, uint32_t *values, uint32_t num_values, 
-		double scale, ros::Time start, double scan_time, bool inverted, std::string frame_id)
+void publish_scan(ros::Publisher *pub, uint32_t *range_values,
+                  uint32_t n_range_values, uint32_t *intensity_values,
+                  uint32_t n_intensity_values, double scale, ros::Time start,
+                  double scan_time, bool inverted, float angle_min,
+                  float angle_max, std::string frame_id)
 {
   static int scan_count = 0;
-  static double last_print_time = 0;
   sensor_msgs::LaserScan scan_msg;
   scan_msg.header.frame_id = frame_id;
   scan_count++;
-  ros::Time t = start;
-  double t_d = t.toSec();
-  if (t_d > last_print_time + 1)	{
-    last_print_time = t_d;
-//    printf("publishing scan %d, inverted=%d\n", scan_count, inverted);
-  }
   if (inverted) {
-    scan_msg.angle_min = M_PI/2;
-    scan_msg.angle_max = -M_PI/2;
+    scan_msg.angle_min = angle_max;
+    scan_msg.angle_max = angle_min;
   } else {
-    scan_msg.angle_min = -M_PI/2;
-    scan_msg.angle_max = M_PI/2;
+    scan_msg.angle_min = angle_min;
+    scan_msg.angle_max = angle_max;
   }
-  scan_msg.angle_increment = (scan_msg.angle_max - scan_msg.angle_min) / (double)(num_values-1);
+  scan_msg.angle_increment = (scan_msg.angle_max - scan_msg.angle_min) / (double)(n_range_values-1);
   scan_msg.scan_time = scan_time;
-  scan_msg.time_increment = scan_time / (double)(num_values-1);
+  scan_msg.time_increment = scan_time / (2*M_PI) * scan_msg.angle_increment;
   scan_msg.range_min = 0;
   if (scale == 0.01) {
     scan_msg.range_max = 81;
@@ -68,22 +65,17 @@ void publish_scan(ros::Publisher *pub, uint32_t *values, uint32_t num_values,
   else if (scale == 0.001) {
     scan_msg.range_max = 8.1;
   }
-  //	scan_msg.set_ranges_size(num_values);
-  scan_msg.ranges.resize(num_values);
-  //  scan_msg.intensities.resize(num_values);
-  scan_msg.header.stamp = t;
-  for (size_t i = 0; i < num_values; i++) {
-    scan_msg.ranges[i] = (float)values[i] * (float)scale;
-    //    scan_msg.intensities[i] = 0;
+  scan_msg.ranges.resize(n_range_values);
+  scan_msg.header.stamp = start;
+  for (size_t i = 0; i < n_range_values; i++) {
+    scan_msg.ranges[i] = (float)range_values[i] * (float)scale;
   }
-  /*
+  scan_msg.intensities.resize(n_intensity_values);
+  for (size_t i = 0; i < n_intensity_values; i++) {
+    scan_msg.intensities[i] = (float)intensity_values[i];
+  }
+  
 
-  static double prev_time = 0;
-  double cur_time = ros::Time::now().toSec();
-  if (rand() % 10 == 0)
-  printf("%f\n", cur_time - prev_time);
-  prev_time = cur_time;
-  */
   pub->publish(scan_msg);
 }
 
@@ -93,10 +85,13 @@ int main(int argc, char **argv)
 	string port;
 	int baud;
 	bool inverted;
-	int angle;
-	double resolution;
+  int angle;
+  double resolution;
 	std::string frame_id;
 	double scan_time = 0;
+  double angle_increment = 0;
+  float angle_min = 0.0;
+  float angle_max = 0.0;
 
 	ros::NodeHandle nh;
 	ros::NodeHandle nh_ns("~");
@@ -104,7 +99,7 @@ int main(int argc, char **argv)
 	nh_ns.param("port", port, string("/dev/lms200"));
 	nh_ns.param("baud", baud, 38400);
 	nh_ns.param("inverted", inverted, false);
-	nh_ns.param("angle", angle, 0);
+  nh_ns.param("angle", angle, 0);
 	nh_ns.param("resolution", resolution, 0.0);
 	nh_ns.param<std::string>("frame_id", frame_id, "laser");
 
@@ -114,21 +109,40 @@ int main(int argc, char **argv)
 		ROS_ERROR("Baud rate must be in {9600, 19200, 38400, 500000}");
 		return 1;
 	}
-	uint32_t values[SickLMS::SICK_MAX_NUM_MEASUREMENTS] = {0};
-	uint32_t num_values = 0;
+	uint32_t range_values[SickLMS::SICK_MAX_NUM_MEASUREMENTS] = {0};
+  uint32_t intensity_values[SickLMS::SICK_MAX_NUM_MEASUREMENTS] = {0};
+	uint32_t n_range_values = 0;
+  uint32_t n_intensity_values = 0;
 	SickLMS sick_lms(port);
 	double scale = 0;
+  double angle_offset;
+  uint32_t partial_scan_index;
 
 	try
 	{
 		sick_lms.Initialize(desired_baud);
-		if (angle == 0)
+
+                // Set the angle and resolution if possible (not an LMSFast) and
+                // the user specifies a setting.
+                if (sick_lms.IsSickLMSFast()) {
+                }
+                if (angle == 0)
                   angle = sick_lms.GetSickScanAngle();
                 if (resolution == 0.0)
                   resolution = sick_lms.GetSickScanResolution();
-                sick_lms.SetSickVariant(sick_lms.IntToSickScanAngle(angle),
-			sick_lms.DoubleToSickScanResolution(resolution));
-                ROS_INFO("Setting variant to (%i, %f)", angle, resolution);
+                try {
+                    ROS_INFO("Setting variant to (%i, %f)", angle, resolution);
+                    sick_lms.SetSickVariant(sick_lms.IntToSickScanAngle(angle),
+                                            sick_lms.DoubleToSickScanResolution(resolution));
+                } catch (SickConfigException e) {
+                  int actual_angle = sick_lms.GetSickScanAngle();
+                  double actual_resolution = sick_lms.GetSickScanResolution();
+                  if (angle != actual_angle)
+                    ROS_WARN("Unable to set scan angle. Using %i instead of %i.", actual_angle, angle);
+                  if (resolution != actual_resolution)
+                    ROS_WARN("Unable to set resolution. Using %e instead of %e.", actual_resolution, resolution);
+                }
+
 		SickLMS::sick_lms_measuring_units_t u = sick_lms.GetSickMeasuringUnits();
 		if (u == SickLMS::SICK_MEASURING_UNITS_CM)
 			scale = 0.01;
@@ -140,34 +154,107 @@ int main(int argc, char **argv)
 			return 1;
 		}
 
-		double scan_res = sick_lms.GetSickScanResolution();
-		SickLMS::sick_lms_scan_resolution_t scan_resolution = SickLMS::DoubleToSickScanResolution(scan_res);
+    // The scan time is always 1/75 because that's how long it takes
+    // for the mirror to rotate. If we have a higher resolution, the
+    // SICKs interleave the readings, so the net result is we just
+    // shift the measurements.
+    if (angle == 180 || sick_lms.IsSickLMSFast()) {
+      scan_time = 1.0 / 75;
+    } 
+    else {
+      SickLMS::sick_lms_scan_resolution_t scan_resolution =
+        SickLMS::DoubleToSickScanResolution(resolution);
+      if ( scan_resolution == SickLMS::SICK_SCAN_RESOLUTION_25) {
+        // 0.25 degrees
+        scan_time = 4.0 / 75;   // 53.33 ms
+      }
+      else if ( scan_resolution == SickLMS::SICK_SCAN_RESOLUTION_50) {
+        // 0.5 degrees
+        scan_time = 2.0 / 75;   // 26.66 ms
+      }
+      else if ( scan_resolution == SickLMS::SICK_SCAN_RESOLUTION_100) {
+        // 1 degree
+        scan_time = 1.0 / 75;   // 13.33 ms
+      }
+      else {
+        ROS_ERROR("Bogus scan resolution.");
+        return 1;
+      }
+      if ( scan_resolution != SickLMS::SICK_SCAN_RESOLUTION_100) {
+        ROS_WARN("You are using an angle smaller than 180 degrees and a "
+                 "scan resolution less than 1 degree per scan. Thus, "
+                 "you are in inteleaved mode and the returns will not "
+                 "arrive sequentially how you read them. So, the "
+                 "time_increment field will be misleading. If you need to "
+                 "know when the measurement was made at a time resolution "
+                 "better than the scan_time, use the whole 180 degree "
+                 "field of view.");
+      }
+    }
 
-		if ( scan_resolution == SickLMS::SICK_SCAN_RESOLUTION_25) {  // 0.25 degrees
-			scan_time = 4.0 / 75;   // 53.33 ms
-		}
-		else if ( scan_resolution == SickLMS::SICK_SCAN_RESOLUTION_50) {  // 0.5 degrees
-			scan_time = 2.0 / 75;   // 26.66 ms
-		}
-		else if ( scan_resolution == SickLMS::SICK_SCAN_RESOLUTION_100) { // 1 degree
-			scan_time = 1.0 / 75;   // 13.33 ms
-		}
-		else {
-			ROS_ERROR("Bogus scan resolution.");
-			return 1;
-		}
+    // The increment for the slower LMS is still 1.0 even if its set to
+    // 0.5 or 0.25 degrees resolution because it is interleaved. So for
+    // 0.5 degrees, two scans are needed, offset by 0.5 degrees. These
+    // show up as two seperate LaserScan messages.
+    angle_increment = sick_lms.IsSickLMSFast() ? 0.5 : 1.0;
+
+    angle_offset = (180.0-angle)/2;
 	}
 	catch (...)
 	{
-		ROS_ERROR("Initialise failed! are you using the correct device path?");
+		ROS_ERROR("Initialize failed! are you using the correct device path?");
+    return 2;
 	}
 	try
 	{
+    ros::Time last_scan_time = ros::Time::now();
 		while (ros::ok())
 		{
-			ros::Time start = ros::Time::now();
-			sick_lms.GetSickScan(values, num_values);
-			publish_scan(&scan_pub, values, num_values, scale, start, scan_time, inverted, frame_id);
+      if (sick_lms.IsSickLMSFast()) {
+        // There's no inteleaving, but we can capture both the range
+        // and intensity simultaneously
+        sick_lms.GetSickScan(range_values, intensity_values,
+                             n_range_values, n_intensity_values);
+        angle_min = -M_PI/4;
+        angle_max = M_PI/4;
+      }
+      else if (angle != 180) {
+        // If the angle is not 180, we can't get partial scans as they
+        // arrive. So, we have to wait for a full scan to get
+        // there. 
+        sick_lms.GetSickScan(range_values, n_range_values);
+        angle_min = (-90.0 + angle_offset) * M_PI / 180.0;
+        angle_max = (90.0 - angle_offset)  * M_PI / 180.0;
+      }
+      else {
+        // We get scans that could be potentially interleaved
+        // depending on the mode. We want to stream out the data as
+        // soon as we get it otherwise the timing won't work right to
+        // reconstruct the data if the sensor is moving.
+        sick_lms.GetSickPartialScan(range_values, n_range_values,
+                                    partial_scan_index);
+        double partialScanOffset = 0.25 * partial_scan_index;
+        angle_min = (-90.0 + angle_offset + partialScanOffset) * M_PI / 180.0;
+        angle_max = (90.0 - angle_offset - fmod(1.0 - partialScanOffset, 1.0))
+          * M_PI / 180.0;
+      }
+      // Figure out the time that the scan started. Since we just
+      // fished receiving the data, we'll assume that the mirror is at
+      // 180 degrees now, or half a scan time. In other words, we
+      // assume a zero transfer time of the data.
+      ros::Time end_of_scan = ros::Time::now();
+      ros::Time start = end_of_scan - ros::Duration(scan_time / 2.0);
+
+      ros::Duration diff = start - last_scan_time;
+      if (diff > ros::Duration(scan_time * 1.5)) {
+        ROS_WARN_STREAM("A scan was probably missed. The last scan was "
+                        << diff.toSec() << " seconds ago.");
+      }
+      last_scan_time = start;
+
+			publish_scan(&scan_pub, range_values, n_range_values, intensity_values,
+                   n_intensity_values, scale, start, scan_time, inverted,
+                   angle_min, angle_max, frame_id);
 			ros::spinOnce();
 		}
 	}
